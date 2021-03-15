534726d13fb39f4e47189a0fcfc9b39e42b52019
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-15 15:38:41 +0300

    copy nursing

---
+++ Pharmacy/Resource/Pharmacy-Thermo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Pharmacy-Thermo-Bridging-Header.h
+//  Pharmacy
+//
+//  Created by Andrey Chernyshev on 14.03.2021.
+//
+
+#ifndef Pharmacy_Thermo_Bridging_Header_h
+#define Pharmacy_Thermo_Bridging_Header_h
+
+#import <RushSDK/RushSDK.h>
+
+#endif /* Pharmacy_Thermo_Bridging_Header_h */
