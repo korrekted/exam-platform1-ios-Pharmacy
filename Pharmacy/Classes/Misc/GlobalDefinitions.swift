082640cf5a44be05451dbcf53ab6943476ff6b9a
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-18 11:27:10 +0300

    build 9

--- Pharmacy/Classes/Misc/GlobalDefinitions.swift
+++ Pharmacy/Classes/Misc/GlobalDefinitions.swift
@@ -7,17 +7,17 @@
 
 final class GlobalDefinitions {
     static let domainUrl = "https://exams.korrekted.com"
-    static let apiKey = "cJTI3sIiSExhjG90"
+    static let apiKey = "HhB39BNssFA5YffN"
 
     static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "WIJ4ZZqtgxDOHPqR"
+    static let sdkApiKey = "7eroaUhf9ULKleIo"
 
-    static let applicationTag = "EXAM_FNP"
+    static let applicationTag = "EXAM_PHARMACY"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appleAppID = "1572754729"
+    static let appleAppID = "1558309982"
 
     static let termsOfServiceUrl = "https://docs.google.com/document/d/1-W9vqg-sno9DcYcDo-XhT26TMhYE1mOU6OivK2IhI7M/edit"
     static let privacyPolicyUrl = "https://docs.google.com/document/d/1GCIelmdgxFi7_dLgg0O_RzfHZB3PMYlczk9gekCRlIE/edit"
